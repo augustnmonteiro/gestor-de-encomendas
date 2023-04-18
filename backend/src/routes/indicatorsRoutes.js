4634852edf4f9cf6a923f226481511bca0406fd7
ivinakarol <ivina.karol@gmail.com>
2023-04-18 10:56:05 -0300

    adicionando o backend dos indicadores

--- backend/src/routes/indicatorsRoutes.js
+++ backend/src/routes/indicatorsRoutes.js
@@ -1,15 +1,47 @@
 const express = require('express');
 const router = express.Router();
 
-// router.get('/list', (req, res) => {
-// })
-// router.get('/list/:id', (req, res) => {
-// })
-// router.post('/post', (req, res) => {
-// })
-// router.put('/put/:id', (req, res) => {
-// })
-// router.delete('/delete/:id', (req, res) => {
-// })
+
+router.get('/sum', (req, res) => {
+    let query = "SELECT SUM(weight) FROM orders";
+    if (req.query.showDeleted != "1") {
+      query += ' WHERE deleted_at IS NULL';
+    }
+    req.connection.query(query, (error, result) => {
+      if (error) {
+        res.status(404).send();
+      } else {
+        res.send(result);
+      }
+    });
+  })
+  
+router.get('/count', (req, res) => {
+  let query = "SELECT COUNT(id) FROM orders";
+  if (req.query.showDeleted != "1") {
+    query += ' WHERE deleted_at IS NULL';
+  } 
+  req.connection.query(query, (error, result) => {
+    if (error) {
+      res.status(404).send();
+    } else {
+      res.send(result);
+    }
+  });
+})
+router.get('/groupBy', (req, res) => {
+  let query = "SELECT status, count(id) as number_status FROM orders GROUP BY status";
+  
+  req.connection.query(query, (error, result) => {
+    if (req.query.showDeleted != "1") {
+      query += ' WHERE deleted_at IS NULL';
+    } 
+    if (error) {
+      res.status(404).send();
+    } else {
+      res.send(result);
+    }
+  });
+})
 
 module.exports = router;
