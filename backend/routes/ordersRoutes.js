b9f68b86917bb619448da9f33d9030ac562f5f79
Vanessa rodrigues <vanessa.rl1036@gmail.com>
2023-04-18 06:00:51 -0300

    Add route para alteração de status

--- backend/routes/ordersRoutes.js
+++ backend/routes/ordersRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+
 //Metodos (demonstração)
 router.get('/list', (req, res) => {
 })
@@ -8,8 +9,24 @@ router.get('/list/:id', (req, res) => {
 })
 router.post('/post', (req, res) => {
 })
+
 router.put('/put/:id', (req, res) => {
-})
+    const newstatus = req.body.status;
+    let query = `UPDATE orders SET status='${newstatus}' WHERE id='${req.params.id}'`;
+    if (newstatus) {
+        //ATUALIZAR NO BANCO
+        req.connection.query(query, (error, result) => {
+                if (error) {
+                    res.status(500).send("SERVER ERROR");
+                } else {
+                    res.status(201).send("UPDATED");
+                }
+            });
+    } else {
+        res.status(400).send("BAD REQUEST");
+    }
+});
+
 router.delete('/delete/:id', (req, res) => {
 })
 
--- backend/src/main.js
+++ backend/src/main.js
@@ -3,8 +3,8 @@ const express = require('express');
 const app = express();
 app.use(express.json())
 //Routers
-const indicatorsRouter = require('./routes/indicatorsRoutes');
-const ordersRouter = require('./routes/ordersRoutes');
+const indicatorsRouter = require('../routes/indicatorsRoutes');
+const ordersRouter = require('../routes/ordersRoutes');
 //Server
 const mysql = require('mysql2');
 const port = 3001;
