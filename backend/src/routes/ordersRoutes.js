ef0ff6d800a4f1c4b3fbcdc25655e7e6c1c7ba69
David Freitas <davidfreitasqa@gmail.com>
2023-04-20 19:56:30 -0300

    Atualização da Rota OrderOrders

--- backend/src/routes/ordersRoutes.js
+++ backend/src/routes/ordersRoutes.js
@@ -33,26 +33,14 @@ router.get('/list/:id', (req, res) => {
 //ROUTER PARA PESQUISAR ENCOMENDAS (searchOrders)
 
 //ROUTER PARA ORDENAR ENCOMENDAS (orderOrders)
-router.get('/ascname', (req, res) => {
-    let query = `SELECT * FROM orders`
+router.get('/list/:order', (req, res) => {
+    let query = `SELECT * FROM orders`;
     if (req.query.showDeleted != "1") {
-        query += ` WHERE deleted_at IS NULL ORDER BY name ASC`
-    }
-    req.connection.query(query, (error, result) => {
-        if (error) {
-            res.status(404).send();
-            console.log(error);
-        } else {
-            res.send(result);
-            console.log(query);
+        if(req.params.order == 'asc'){
+            query += ` WHERE deleted_at IS NULL ORDER BY name ASC`
+        }else if(req.params.order == 'desc'){
+            query += ` WHERE deleted_at IS NULL ORDER BY name DESC`
         }
-    })
-})
-
-router.get('/descname', (req, res) => {
-    let query = `SELECT * FROM orders`
-    if (req.query.showDeleted != "1") {
-        query += ` WHERE deleted_at IS NULL ORDER BY name DESC`
     }
     req.connection.query(query, (error, result) => {
         if (error) {
@@ -61,6 +49,7 @@ router.get('/descname', (req, res) => {
         } else {
             res.send(result);
             console.log(query);
+            c
         }
     })
 })
