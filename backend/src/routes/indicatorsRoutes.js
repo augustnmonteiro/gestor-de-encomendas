a9953bbf004202172443ec432b6ded125211fe2b
ivinakarol <ivina.karol@gmail.com>
2023-04-20 20:51:10 -0300

    Correção do back dos indicadores

--- backend/src/routes/indicatorsRoutes.js
+++ backend/src/routes/indicatorsRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 
 router.get('/total-weight', (req, res) => {
-    let query = "SELECT SUM(weight) as total FROM orders";
+    const query = "SELECT SUM(weight) as total FROM orders";
     req.connection.query(query, (error, result) => {
       if (error) {
         res.status(404).send();
@@ -14,7 +14,7 @@ router.get('/total-weight', (req, res) => {
   })
   
 router.get('/total-orders', (req, res) => {
-  let query = "SELECT COUNT(id) as total FROM orders";
+  const query = "SELECT COUNT(id) as total FROM orders";
   req.connection.query(query, (error, result) => {
     if (error) {
       res.status(404).send();
@@ -24,7 +24,7 @@ router.get('/total-orders', (req, res) => {
   });
 })
 router.get('/total-orders-by-status', (req, res) => {
-  let query = "SELECT status, count(id) as total FROM orders GROUP BY status";
+  const query = "SELECT status, count(id) as total FROM orders GROUP BY status";
   
   req.connection.query(query, (error, result) => {
     if (error) {
