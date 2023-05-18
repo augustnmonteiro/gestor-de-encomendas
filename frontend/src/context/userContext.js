937f406c0899dc8c47b0f1df58e0e0e035d1fcbd
Hugo Silva <hugo3az@gmail.com>
2023-05-18 09:14:36 -0300

    Adicionando modificações solicitadas

--- frontend/src/context/userContext.js
+++ frontend/src/context/userContext.js
@@ -4,12 +4,11 @@ export const UserContext = React.createContext();
 
 export const UserDataContext = ({ children }) => {
     const [listOrders, setListOrders] = React.useState([]);
-    const [listOrdersComplet, setListOrdersComplet] = React.useState([]);
 
   return (
     <UserContext.Provider
       value={{
-        listOrders, setListOrders, listOrdersComplet, setListOrdersComplet
+        listOrders, setListOrders
       }}
     >
       {children}
