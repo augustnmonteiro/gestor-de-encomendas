58ef0cf57ac894513015a6167ad9241fa1c66143
David Freitas <davidfreitasqa@gmail.com>
2023-05-24 16:09:00 -0300

    Implementando pagina HOME, Atualizando Navbar e DropdownOrder

--- frontend/src/components/header/Nav.js
+++ frontend/src/components/header/Nav.js
@@ -1,21 +1,25 @@
-import '../header/Nav.css';
+import "../header/Nav.css";
 import { Link } from "react-router-dom";
-import logo from '../../logo.png'
+import logo from "./img/logo.png";
 
 function Navbar() {
     return (
         <div className="header">
-            <div className="logo-header"><Link to="/"><img src={logo}/></Link></div>
+            <div className="logo-header">
+                <Link to="/">
+                    <img src={logo} />
+                </Link>
+            </div>
             <nav>
                 <ul className="list-header">
                     <li>
                         <Link to="/">Home</Link>
                     </li>
-                     <li>
-                        <Link to="/packages/add">Cadastro</Link>
+                    <li>
+                        <Link to="/packages">Ver Encomendas</Link>
                     </li>
                     <li>
-                        <Link to="/packages">Pesquisar</Link>
+                        <Link to="/packages/add">Cadastrar Encomenda</Link>
                     </li>
                 </ul>
             </nav>
